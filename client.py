22171eb622b46e6b6efc44e4e63e124200e06a8e
tri10 <shubhi.blog@gmail.com>
2020-04-24 20:09:14 +0530

    added file transfer and ngrok

--- client.py
+++ client.py
@@ -1,61 +1,110 @@
 import socket
 import threading
+import os
+import tqdm
 
 
 DISCONNECT = '!!F'
 FORMAT = 'utf-8'
 HEADER = 64
-PORT = 5050
-''' Set the ip of local server '''
+BUFFER = 1024
+''' Port provided by ngrok '''
+PORT = int(input('PORT : '))
+''' Setting ngrok server '''
+SERVER = '0.tcp.ngrok.io'
+# SERVER = '127.0.0.1'
 
-SERVER = '192.168.43.88'
-ADDR = (SERVER,PORT)
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(ADDR)
-print('[SUCCESS] Succesfully connected to ',ADDR)
-nickname = input('Nickname (Max 256 chars): ').encode(FORMAT)
-client.send(nickname)
-connected = True
+ADDR = (SERVER, PORT)
+SEPERATOR = ':'
+
+''' Transferring File '''
+def file_transfer():
+    sendto = input('Send to [everyone,username]: ')
+    file_path = input('Path :')
+    if not os.path.exists(file_path):
+        print('file not found ,try again.. ')
+        file_transfer()
+    
+    filename = os.path.basename(file_path)
+    filesize = os.path.getsize(file_path)
+    send(filename+SEPERATOR+str(filesize)+SEPERATOR+sendto)
+    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    ''' reading data as binary in chunks and sending it to server '''
+    with open(file_path,'rb') as f:
+        for _ in progress:
+            data = f.read(BUFFER)
+            if not data:
+                break
+            client.sendall(data)
+            progress.update(len(data))
+
+''' protocol for sending message to server'''
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_len = len(message)
+    send_length = str(msg_len).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+    if msg == DISCONNECT:
+        print('Disconnected !')
+        client.close()
+        return False
+    if msg in FMSG.keys():
+        FMSG[msg]()
+    return True
+
+''' Recieving File '''
+def rec_file(conn):
+    ln = conn.recv(HEADER).decode(FORMAT)
+    ln = int(ln)
+    recs = conn.recv(ln).decode(FORMAT)
+    filename,filesize = recs.split(SEPERATOR)
+    filesize = int(filesize)
+    with open(filename,'wb') as f:
+        for _ in range(0,filesize,BUFFER):
+            chunk = conn.recv(BUFFER)
+            if not chunk:
+                break
+            f.write(chunk)
+
+''' recieving messages '''
 
 def recieving():
-	try:
-		msg_length = client.recv(HEADER).decode(FORMAT)
-	except socket.error:
-		return 0
-	if msg_length:
-		msg_length = int(msg_length)
-		msg = client.recv(msg_length).decode(FORMAT)
-		print('\r'+msg+'\nYou : ',end='')
-
-
-
-
-def send(msg):
-
-	message = msg.encode(FORMAT)
-	msg_len = len(message)
-	send_length = str(msg_len).encode(FORMAT)
-	send_length += b' '*(HEADER - len(send_length))
-	client.send(send_length)
-	client.send(message)
-
-	if msg == DISCONNECT:
-		print('Disconnected !')
-		client.close()
-		return False
-	return True
+    try:
+        msg_length = client.recv(HEADER).decode(FORMAT)
+    except socket.error:
+        return 0
+    if msg_length:
+        msg_length = int(msg_length)
+        msg = client.recv(msg_length).decode(FORMAT)
+        if msg in FMSG:
+            FMSG[msg](client)
+        else:	
+            print('\r' + msg + '\nYou : ', end='')
 
 def user_msgs():
-	while connected:
-		recieving()
+    while connected:
+        recieving()
 
 def chat():
-	global connected
-	while connected:
-		msg = input('You : ')
-		o = send(msg)
-		if not o:
-			connected = False
-usr_thread = threading.Thread(target=user_msgs)
-usr_thread.start()
-chat()
+    global connected
+    while connected:
+        msg = input('You : ')
+        o = send(msg)
+        if not o:
+            connected = False
+
+FMSG = {'!!T':file_transfer,'--R':rec_file}
+if __name__ == '__main__':
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    print('[SUCCESS] Succesfully connected to ', ADDR)
+    nickname = input('Nickname (Max 64 chars): ').encode(FORMAT)
+    nickname += b' ' * (HEADER - len(nickname))
+    client.send(nickname)
+    connected = True
+    usr_thread = threading.Thread(target=user_msgs)
+    usr_thread.start()
+    chat()
--- server.py
+++ server.py
@@ -2,23 +2,68 @@ import socket
 import threading
 import random
 import string
+from pyngrok import ngrok
+import tqdm
 
 ''' Initializing variables '''
-
 DISCONNECT = '!!F'
+BUFFER = 1024
 FORMAT = 'utf-8'
 HEADER = 64
 PORT = 5050
+SEPERATOR = ':'
+connections = {}
 ''' generating random keyword for join message'''
 JOIN = ''.join([random.choice(string.ascii_uppercase) for _ in range(random.randrange(30,50))])
-'''  Socket obj for server '''
-SERVER = socket.gethostbyname(socket.gethostname())
+'''  Local host where traffic would be forwarded by ngrok server '''
+SERVER = '127.0.0.1'
 ADDR = (SERVER,PORT)
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 ''' Reusing the same port if rerun '''
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#  FORWARDING TRAFFIC TO LOCAL HOST:PORT
+p_url = ngrok.connect(PORT,'tcp')
+_,n_host,n_port = p_url.split(':')
 server.bind(ADDR)
-connections = {}
+
+''' Random Client Generator '''
+def gen_username():
+	n = 1
+	while 1:
+		yield f'user {n}'
+		n +=1
+
+gen = gen_username()
+
+
+
+''' recieving file '''
+def transfer_file(conn,name):
+	ln = conn.recv(HEADER).decode(FORMAT)
+	ln = int(ln)
+	recs = conn.recv(ln).decode(FORMAT)
+	filename,filesize,nick = recs.split(SEPERATOR)
+	for con in connections.values():
+		if con[1] == nick:
+			sender = con[0]
+			print("Sending file to ",nick)
+			send(sender,'--R')
+			send(sender,filename+SEPERATOR+str(filesize))
+			break
+	else:
+		print('user not found !')
+		conn.close()
+
+	filesize = int(filesize)
+	for _ in range(0,filesize,BUFFER):
+		chunk = conn.recv(BUFFER)
+		if not chunk:
+			break
+		sender.send(chunk)
+	send(sender,f'{name} : [FILE] {filename} ')
+
+
+
 
 ''' Sending messages to all connected users '''
 def sendtoall(msg,addr,nickname):
@@ -28,7 +73,7 @@ def sendtoall(msg,addr,nickname):
 		message = (f'[JOINED] {nickname}').encode(FORMAT)
 	
 	else:
-		message = (f'[MESSAGE] {nickname} : {msg}').encode(FORMAT)
+		message = (f'{nickname} : {msg}').encode(FORMAT)
 	msg_len = str(len(message)).encode(FORMAT)
 	msg_len += b' '*(HEADER - len(msg_len))
 	for addresses in connections:
@@ -40,7 +85,16 @@ def sendtoall(msg,addr,nickname):
 				del connections[addresses]
 
 
-''' Handling and showing clients messages ''' 
+''' Sending msg  '''
+def send(conn,msg):
+    message = msg.encode(FORMAT)
+    msg_len = len(message)
+    send_length = str(msg_len).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    conn.send(send_length)
+    conn.send(message)
+
+''' Handling and Sending messages to all clients ''' 
 def handle_client(conn,addr):
 	sendtoall(JOIN,addr,connections[addr][1])
 	connection = True
@@ -49,9 +103,12 @@ def handle_client(conn,addr):
 		if msg_length:
 			msg_length = int(msg_length)
 			msg = conn.recv(msg_length).decode(FORMAT)
-			sendtoall(msg,addr,connections[addr][1])
 			if msg == DISCONNECT:
 				connection = False
+			if msg in FMSG:
+				FMSG[msg](conn,connections[addr][1])
+				continue
+			sendtoall(msg, addr, connections[addr][1])
 
 	del connections[addr]
 	conn.close()
@@ -59,21 +116,31 @@ def handle_client(conn,addr):
 
 
 
-''' Starting Listening on IP '''
+''' Starting Listening on Server Socket '''
 def start():
 	server.listen()
 	print(f'[LISTENING] Server listening on  {SERVER}')
+	print('HOST : ',n_host)
+	print('PORT : ',n_port)
 	while True:
 		conn,addr = server.accept()
-		nick = conn.recv(256).decode(FORMAT)
+		try:
+			nick = conn.recv(HEADER).decode(FORMAT).strip()
+		except:
+			continue
+		else:
+			if not nick:
+				continue
+
 		connections[addr] = (conn,nick)
 		thread = threading.Thread(target=handle_client,args=(conn,addr))
-		print(f'[JOINED] {nick} join the  chat')
+		print(f'[JOINED] {nick} join the  chat  ->',addr)
 		print(f'Current Connections : {threading.activeCount()}')
 		thread.start()
 
 		
-''' Handling Force exit and closing server '''
+FMSG = {'!!T':transfer_file}
+''' Handling Force exit and closing server connection '''
 try:
 	start()
 except KeyboardInterrupt:
